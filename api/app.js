293fbe3ade92361d51dd8b178a64adfd037d79ff
alec-somers <alecv1528@gmail.com>
2025-09-03 11:18:28 -0700

    Vehicles endpoint added

--- api/app.js
+++ api/app.js
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
   res.send("App is up and running.");
 });
 
+app.get("/vehicles", (req, res) => {
+    res.send("Welcome to vehicles!");
+});
+
 app.listen(port, () => {
   console.log("Your knex and express app running successfully!");
 });
