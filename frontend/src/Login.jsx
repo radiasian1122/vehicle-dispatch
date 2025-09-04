e26ff84980bdc347a39c072a477eade67752aeeb
alec-somers <alecv1528@gmail.com>
2025-09-04 11:23:37 -0700

    styling added to login page

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,7 +1,27 @@
 import "../styles/Login.css"
+import {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 
 function Login() {
+
+
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const submitData = {
+            username: event.target[0].value,
+            password: event.target[1].value
+        }
+
+
+
+    }
+
+
+
+
     return (
         <>
             <div className="main-container flex">
@@ -11,15 +31,16 @@ function Login() {
                 <div className="form-container flex">
                     <form className="login-form flex">
                         <div>
-                            <label htmlFor="username-input">Username:</label>
+                            <label htmlFor="username-input form-label">Username:</label>
                         </div>
                         <input type="text" id="username-input" className="form-input"/>
 
                         <div>
-                            <label htmlFor="password-input">Password:</label>
+                            <label htmlFor="password-input form-label">Password:</label>
                         </div>
                         <input type="password" id="password-input" className="form-input"/>
 
+                        <button type="submit" className="login-button" onSubmit={ (e) => handleSubmit(e)}>Login</button>
                     </form>
                 </div>
 
