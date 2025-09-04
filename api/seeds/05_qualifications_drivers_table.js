77090ed82b4a0f34c91cc4cae4fda6207abde19a
alec-somers <alecv1528@gmail.com>
2025-09-03 21:35:39 -0700

    seed files fixed

--- api/seeds/05_qualifications_drivers_table.js
+++ api/seeds/05_qualifications_drivers_table.js
@@ -1,4 +1,5 @@
 const { faker } = require('@faker-js/faker')
+const {generateDrivers} = require('../utils.js')
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
@@ -6,17 +7,5 @@ const { faker } = require('@faker-js/faker')
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('qualifications_drivers').del()
-    const qualDrivers = [];
-    for (let i = 0; i < 200; i++){
-        for (let j = 0; j < 9; i++){
-            qualDrivers.push({
-                driver_id: i + 1,
-                qualification_id: j + 1
-            })
-
-
-        }
-    }
-
-  await knex('qualifications_drivers').insert(noDuplicates);
+  await knex('qualifications_drivers').insert(generateDrivers());
 };
