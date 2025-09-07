b53ba1f671c00c141caeac7da6bef55551379f1c
alec-somers <alecv1528@gmail.com>
2025-09-07 14:45:39 -0700

    vehicles component 90% -vehicles component styling added

--- frontend/src/Vehicles.jsx
+++ frontend/src/Vehicles.jsx
@@ -1,9 +1,56 @@
+import {useState, useEffect} from 'react';
+import "../styles/vehicles.css"
 
+function Vehicles() {
 
-function Vehicles () {
+    const [vehicleList, setVehicleList] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:8080/vehicles')
+            .then(res => res.json())
+            .then(vehicles => {
+                const vehicleArray = [];
+
+                for (let vehicle of vehicles) {
+                    vehicleArray.push(vehicle);
+                }
+
+                setVehicleList(vehicleArray)
+            })
+    }, [])
     return (
-        <h1>Vehicles!</h1>
+        <main className="vehicles-component-container flex">
+            <form className="vehicle-list-form">
+                <h2 className="vehicles-component-header">Vehicle Inventory - Select and Add Vehicles</h2>
+                <table className="vehicles-component-table">
+                    <thead>
+                    <tr>
+                        <th>Type</th>
+                        <th>Callsign</th>
+                        <th>Company</th>
+                        <th>Status</th>
+                        <th>Select</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {vehicleList.map((vehicle) => {
+                        return (
+                            <tr>
+                                <td>{vehicle.type}</td>
+                                <td>{vehicle.callsign}</td>
+                                <td>{vehicle.company}</td>
+                                <td>{vehicle.status}</td>
+                                <td><input type="checkbox" name={vehicle.callsign} value={vehicle.id}></input></td>
+                            </tr>
+                        )
+                    })}
+                    </tbody>
+                </table>
+                <button type="submit">Submit</button>
+            </form>
+        </main>
     )
 }
 
 export default Vehicles;
+
